4bdb052aa425ca059d63c48fe4a6ac22a444b54b
sudeepmi <sudeepm0599@gmail.com>
2024-04-03 16:51:23 +0545

    feat: Readme file updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import Image from "next/image";
 import React from "react";
 
 
-export const getProps = (async () => {
+const getProps = (async () => {
   // Fetch data from external API
   const alcho = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list");
   const glasses = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list");
   const categories = await axios.get("https://thecocktaildb.com/api/json/v1/1/list.php?c=list");
   const ingredients = await axios.get("https://thecocktaildb.com/api/json/v1/1/list.php?i=list");
-
-
 
   // Pass data to the page via props
   const pageProps = {
@@ -27,9 +25,8 @@ export default async function Home() {
   const props = await getProps();
 
   return (
-    <main className="">
+    <main className="p-20">
       <div className="grid grid-cols-3">
-    
             <div className="left">
                 <h2>Alcohol Types</h2>
                 <ul>
